231f6d541072b8dba533f363d934292540fd4724
Mikey365272 <zaharma12@gmail.com>
2026-04-12 13:40:00 +0300

    сделал так, чтобы все запускалось через main.py

--- app/routes/students.py
+++ app/routes/students.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, Form, Request
-from fastapi.templating import Jinja2Templates
-from database.db import create_student, get_all_students
 from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from database.db import get_all_students, create_student  # добавь импорты
 
 router = APIRouter()
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory="templates")  # было: templates = None
 
 @router.get('/students')
 def students_page(request: Request):
     students = get_all_students()
-    return templates.TemplateResponse('students/list.html',{'request': request, 'students': students})
+    return templates.TemplateResponse(request, 'students/list.html', {'students': students})
 
 @router.post('/student/add')
 def add_student(name: str = Form(...), class_name: str = Form(...)):
